0ade9185f051bff54b9501a4f6d95807875f9fad
Mishail Nikolaev <mishail.nikolaev2017@yandex.ru>
2024-07-05 14:30:09 +0300

    n

--- ewm-service/src/main/java/ru/practicum/model/event/EventRepository.java
+++ ewm-service/src/main/java/ru/practicum/model/event/EventRepository.java
@@ -4,68 +4,65 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.querydsl.QuerydslPredicateExecutor;
+import org.springframework.data.repository.query.Param;
 
 import java.time.LocalDateTime;
 import java.util.List;
 
 public interface EventRepository extends JpaRepository<Event, Long>, QuerydslPredicateExecutor<Event> {
 
-
     @Query(value = "select * from events e " +
             "join users u on e.initiator = u.id " +
             "join category c on e.category = c.id " +
-            "where u.id in ?1 and e.state in ?2 and c.id in ?3 and e.event_date between ?4 and ?5",
+            "where u.id in :initiatorIds and e.state in :states and c.id in :categoryIds and e.event_date between :rangeStart and :rangeEnd",
             nativeQuery = true)
-    List<Event> getEvents(List<Long> initiatorIds,
-                          List<String> states,
-                          List<Long> categoriesIds,
-                          LocalDateTime rangeStart,
-                          LocalDateTime rangeEnd,
+    List<Event> getEvents(@Param("initiatorIds") List<Long> initiatorIds,
+                          @Param("states") List<String> states,
+                          @Param("categoryIds") List<Long> categoryIds,
+                          @Param("rangeStart") LocalDateTime rangeStart,
+                          @Param("rangeEnd") LocalDateTime rangeEnd,
                           Pageable pageable);
 
     @Query(value = "select * from events e " +
             "join category c on e.category = c.id " +
-            "where lower(e.annotation) like lower(concat('%', ?1, '%')) " +
-            "and e.category in ?2 " +
-            "and e.paid = ?3 " +
-            "and e.event_date between ?4 and ?5",
+            "where lower(e.annotation) like lower(concat('%', :text, '%')) " +
+            "and e.category in :categories " +
+            "and e.paid = :paid " +
+            "and e.event_date between :rangeStart and :rangeEnd",
             nativeQuery = true)
-    List<Event> getEventsByAllParam(String text,
-                                    List<Long> categories,
-                                    Boolean paid,
-                                    LocalDateTime rangeStart,
-                                    LocalDateTime rangeEnd,
+    List<Event> getEventsByAllParam(@Param("text") String text,
+                                    @Param("categories") List<Long> categories,
+                                    @Param("paid") Boolean paid,
+                                    @Param("rangeStart") LocalDateTime rangeStart,
+                                    @Param("rangeEnd") LocalDateTime rangeEnd,
                                     Pageable pageable);
 
     @Query(value = "select * from events e " +
             "join category c on e.category = c.id " +
-            "where lower(e.annotation) like lower(concat('%', ?1, '%')) " +
-            "and e.category in ?2 " +
-            "and e.paid = ?3 ",
+            "where lower(e.annotation) like lower(concat('%', :text, '%')) " +
+            "and e.category in :categories " +
+            "and e.paid = :paid",
             nativeQuery = true)
-    List<Event> getEventsByTextCatPaid(String text,
-                                       List<Long> categories,
-                                       Boolean paid,
+    List<Event> getEventsByTextCatPaid(@Param("text") String text,
+                                       @Param("categories") List<Long> categories,
+                                       @Param("paid") Boolean paid,
                                        Pageable pageable);
 
     @Query(value = "select * from events e " +
             "join category c on e.category = c.id " +
-            "where lower(e.annotation) like lower(concat('%', ?1, '%')) " +
-            "and e.category in ?2 ",
+            "where lower(e.annotation) like lower(concat('%', :text, '%')) " +
+            "and e.category in :categories",
             nativeQuery = true)
-    List<Event> getEventsByTextCat(String text,
-                                   List<Long> categories,
+    List<Event> getEventsByTextCat(@Param("text") String text,
+                                   @Param("categories") List<Long> categories,
                                    Pageable pageable);
 
     @Query(value = "select * from events e " +
-            "where lower(e.annotation) like lower(concat('%', ?1, '%')) ",
+            "where lower(e.annotation) like lower(concat('%', :text, '%'))",
             nativeQuery = true)
-    List<Event> getEventsByText(String text, Pageable pageable);
+    List<Event> getEventsByText(@Param("text") String text, Pageable pageable);
 
     List<Event> findAllEventByInitiatorId(Long initiatorId, Pageable pageable);
 
     Event findByIdAndInitiatorId(Long eventId, Long initiatorId);
-
-
 }
-
