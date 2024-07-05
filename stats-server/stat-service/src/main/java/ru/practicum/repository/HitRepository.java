6f395fa7b0810a821bb4b642de89b178c2cea3bb
Mishail Nikolaev <mishail.nikolaev2017@yandex.ru>
2024-07-05 14:25:08 +0300

    n

--- stats-server/stat-service/src/main/java/ru/practicum/repository/HitRepository.java
+++ stats-server/stat-service/src/main/java/ru/practicum/repository/HitRepository.java
@@ -2,6 +2,7 @@ package ru.practicum.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import ru.practicum.ViewStats;
 import ru.practicum.model.Hit;
 
@@ -11,29 +12,26 @@ import java.util.List;
 public interface HitRepository extends JpaRepository<Hit, Long> {
 
     @Query("select new ru.practicum.ViewStats(s.app, s.uri, count(s.uri)) " +
-            "from Hit s where s.timestamp between ?1 and ?2 " +
+            "from Hit s where s.timestamp between :start and :end " +
             "group by s.app, s.uri " +
             "order by count(s.uri) desc")
-    List<ViewStats> getHits(LocalDateTime start, LocalDateTime end);
+    List<ViewStats> getHits(@Param("start") LocalDateTime start, @Param("end") LocalDateTime end);
 
     @Query("select new ru.practicum.ViewStats(s.app, s.uri, count(s.uri)) " +
-            "from Hit s where s.timestamp between ?1 and ?2 and s.uri in ?3 " +
+            "from Hit s where s.timestamp between :start and :end and s.uri in :uris " +
             "group by s.app, s.uri " +
             "order by count(s.uri) desc")
-    List<ViewStats> getHitsUris(LocalDateTime start, LocalDateTime end, List<String> uris);
-
+    List<ViewStats> getHitsUris(@Param("start") LocalDateTime start, @Param("end") LocalDateTime end, @Param("uris") List<String> uris);
 
     @Query("select new ru.practicum.ViewStats(s.app, s.uri, count(distinct s.ip)) " +
-            "from Hit s where s.timestamp between ?1 and ?2 " +
+            "from Hit s where s.timestamp between :start and :end " +
             "group by s.app, s.uri " +
             "order by count(s.uri) desc")
-    List<ViewStats> getHitsUnique(LocalDateTime start, LocalDateTime end);
+    List<ViewStats> getHitsUnique(@Param("start") LocalDateTime start, @Param("end") LocalDateTime end);
 
     @Query("select new ru.practicum.ViewStats(s.app, s.uri, count(distinct s.ip)) " +
-            "from Hit s where s.timestamp between ?1 and ?2 and s.uri in ?3 " +
+            "from Hit s where s.timestamp between :start and :end and s.uri in :uris " +
             "group by s.app, s.uri " +
             "order by count(s.uri) desc")
-    List<ViewStats> getHitsUrisUnique(LocalDateTime start, LocalDateTime end, List<String> uris);
-
-
+    List<ViewStats> getHitsUrisUnique(@Param("start") LocalDateTime start, @Param("end") LocalDateTime end, @Param("uris") List<String> uris);
 }
