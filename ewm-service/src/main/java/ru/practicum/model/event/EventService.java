63d3c25472cb907a8c51bf80b19ac2860c68e5df
Mishail Nikolaev <mishail.nikolaev2017@yandex.ru>
2024-07-10 22:23:31 +0300

    ghj

---
+++ ewm-service/src/main/java/ru/practicum/model/event/EventService.java
@@ -0,0 +1,57 @@
+package ru.practicum.model.event;
+
+import ru.practicum.model.comment.commentDto.CommentDto;
+import ru.practicum.model.comment.commentDto.NewCommentDto;
+import ru.practicum.model.event.eventDto.EventFullDto;
+import ru.practicum.model.event.eventDto.NewEventDto;
+import ru.practicum.model.event.eventDto.UpdateEventAdminRequest;
+import ru.practicum.model.event.eventDto.UpdateEventUserRequest;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.List;
+
+public interface EventService {
+
+
+    EventFullDto addEvent(NewEventDto newEventDto, Long userId, String path);
+
+
+    EventFullDto updateEventByAdmin(UpdateEventAdminRequest adminEvent, Long eventId);
+
+    List<EventFullDto> getEventsByParam(List<Long> initiatorIds,
+                                        List<String> states,
+                                        List<Long> categoriesIds,
+                                        String rangeStart,
+                                        String rangeEnd,
+                                        Integer from,
+                                        Integer size);
+
+    List<EventFullDto> getEventByInitiator(Long initiatorId,
+                                           Integer from,
+                                           Integer size);
+
+    List<EventFullDto> getEventByParamPublic(String text,
+                                             List<Long> categories,
+                                             Boolean paid,
+                                             String rangeStart,
+                                             String rangeEnd,
+                                             Boolean onlyAvailable,
+                                             String sort,
+                                             Integer from,
+                                             Integer size,
+                                             HttpServletRequest request);
+
+    EventFullDto getEventByIdPublish(Long eventId, HttpServletRequest request);
+
+    EventFullDto getEventByInitiator(Long userId, Long eventId);
+
+    EventFullDto updateEventByInitiator(UpdateEventUserRequest userEvent, Long userId, Long eventId);
+
+    CommentDto createComment(Long eventId, NewCommentDto commentDto, Long userId);
+
+    CommentDto updateCommentByAuthor(NewCommentDto commentDto, Long userId, Long commentId);
+
+    void deleteCommentByUser(Long userId, Long commentId);
+
+    void deleteCommentByAdmin(Long commentId);
+}
---
+++ ewm-service/src/main/java/ru/practicum/model/event/eventDto/EventFullDto.java
@@ -0,0 +1,38 @@
+package ru.practicum.model.event.eventDto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import ru.practicum.model.category.categoryDto.CategoryDto;
+import ru.practicum.model.comment.commentDto.CommentDto;
+import ru.practicum.model.event.State;
+import ru.practicum.model.location.LocationDto;
+import ru.practicum.model.user.userDto.UserShortDto;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class EventFullDto {
+
+    private Long id;
+    private String annotation;
+    private CategoryDto category;
+    private int confirmedRequests;
+    private String createdOn;
+    private String description;
+    private String eventDate;
+    private UserShortDto initiator;
+    private LocationDto location;
+    private Boolean paid;
+    private int participantLimit;
+    private LocalDateTime publishedOn;
+    private Boolean requestModeration;
+    private State state;
+    private String title;
+    private int views;
+    private List<CommentDto> comments;
+
+}
